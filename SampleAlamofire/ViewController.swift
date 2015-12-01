8191e39f2c4465c9a36f7904cfeea718dba12172
masatsugu <paveway.info@gmail.com>
2015-12-01 19:19:30 +0900

    Initial Commit

---
+++ SampleAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleAlamofire
+//
+//  Created by 二俣征嗣 on 2015/12/01.
+//  Copyright © 2015年 Masatsugu Futamata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
